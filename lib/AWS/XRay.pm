e2b2c58acdd4d518b665048802e530b6d5ff66a6
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2018-06-23 00:05:22 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ef6f80f..29424a2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension AWS-XRay
    
     {{$NEXT}}
    
    +0.04 2018-06-22T15:05:03Z
    +    - Add sampler()
    +
     0.03 2018-06-19T16:34:09Z
         - Add sampling_rate()

--- lib/AWS/XRay.pm
+++ lib/AWS/XRay.pm
@@ -12,7 +12,7 @@ use AWS::XRay::Segment;
 use Exporter 'import';
 our @EXPORT_OK = qw/ new_trace_id capture capture_from trace /;
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 our $TRACE_ID;
 our $SEGMENT_ID;
