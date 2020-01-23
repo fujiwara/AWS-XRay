de859ad4d552525775220ad96f80731eac6c250a
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-01-23 09:29:01 +0900

    Checking in changes prior to tagging of version 0.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e72a9a1..1666cfa 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension AWS-XRay
    
     {{$NEXT}}
    
    +0.10 2020-01-23T00:28:43Z
    +    - Fix Bareword (#13 @negachov)
    +
     0.09 2019-08-20T22:01:55Z
         - Support AWS Lambda (#10 @shogo82148)
         - Add EC2 plugin (#6 @papix)

--- lib/AWS/XRay.pm
+++ lib/AWS/XRay.pm
@@ -15,7 +15,7 @@ use AWS::XRay::Buffer;
 use Exporter 'import';
 our @EXPORT_OK = qw/ new_trace_id capture capture_from trace /;
 
-our $VERSION = "0.09";
+our $VERSION = "0.10";
 
 our $TRACE_ID;
 our $SEGMENT_ID;
